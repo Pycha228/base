677c1f2e860f6d373f55eae7133ccea22de2dbf1
Pycha <daniil.kochev.2002@gmail.com>
2024-04-07 22:55:23 +0300

    lesson-74 added (HOMEWORK)

---
+++ lesson-76/main.js
@@ -0,0 +1,18 @@
+// const firstName = 'John';
+// const lastName = 'Smith';
+// let age = 20;
+
+
+
+// console.log(firstName, lastName, age);
+
+
+// ============================================================== Homework
+
+const firstName = 'Daniil';
+const lastName = 'Kochev';
+let age = 23;
+let height = 176;
+let strength = 200000000;
+
+console.log(firstName, lastName, age, "y.o.", strength, height);
