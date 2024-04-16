72685141768ca052c80d93233cbc2af2e15498ef
Pycha <daniil.kochev.2002@gmail.com>
2024-04-16 14:42:58 +0300

    about section adaptive is finished. program section start

--- projects/wake-bake/wake-bake/js/main.js
+++ projects/wake-bake/wake-bake/js/main.js
@@ -1,5 +1,5 @@
 (function () {
-    
+
     document.addEventListener('click', burgerInit)
 
 
@@ -8,31 +8,109 @@
         const burgerIcon = e.target.closest('.burger-icon')
         const burgerNavLink = e.target.closest('.nav__link')
 
-        if(!burgerIcon && !burgerNavLink) return
-        if(document.documentElement.clientWidth > 900) return
+        if (!burgerIcon && !burgerNavLink) return
+        if (document.documentElement.clientWidth > 900) return
 
         if (!document.body.classList.contains('body--opened-menu')) {
             document.body.classList.add('body--opened-menu')
         } else {
             document.body.classList.remove('body--opened-menu')
         }
-
     }
-}) ()
 
-// const burger = document.querySelector('.burger-icon')
-// const menu = document.querySelector('.body')
-// const nav  = document.querySelectorAll('.nav__link')
-// // const cross = document.querySelector('.burger__icon')
-// const body = document.body
+    const modal = document.querySelector('.modal')
+    const modalButton = document.querySelector('.about__img-button')
 
-// burger.addEventListener('click', () => {
-//     menu.classList.toggle('body--opened-menu')
-// })
+    modalButton.addEventListener('click', event => {
+        event.preventDefault()
+        document.body.classList.toggle('body--opened-modal')
+    })
+
+    modal.addEventListener('click', event => {
+        event.preventDefault()
+
+        const target = event.target
+
+        if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+            document.body.classList.remove('body--opened-modal')
+        }
+    })
+
+    document.addEventListener('keydown', event => {
+        if(event.target.code = 'Escape' && document.body.classList.contains('body--opened-modal')) {
+            document.body.classList.remove('body--opened-modal')
+        }
+    })
+
+})()
 
 
 
-// nav.addEventListener('click', () => {
-//     menu.classList.remove('body--opened-menu')
-// })
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ===================Modal capcakes==================================================
+
+
+// const modal = document.querySelector('.modal')
+//     const modalButton = document.querySelector('.about__img-button')
+
+//     modalButton.addEventListener('click', event => {
+//         event.preventDefault()
+//         document.body.classList.toggle('body--opened-modal')
+//     })
+//     modal.addEventListener('click', event => {
+//         event.preventDefault()
+
+//         const target = event.target
+
+//         if(target.closest('.modal__cancel') || target.classList.contains('modal')) {
+//             document.body.classList.remove('body--opened-modal')
+//         }
+//     })
+
+
+// =====================================================================
+
+
+// const modal = document.querySelector('.modal')
+// const modalButton = document.querySelector('.about__img-button')
+
+// modalButton.addEventListener('click', openModal)
+// modal.addEventListener('click', closeModal)
+
+// function openModal (e) {
+//     e.preventDefault()
+//     document.body.classList.toggle('body--opened-modal')
+// }
+
+// function closeModal (e) {
+//     e.preventDefault()
+
+//     const target = e.target
+
+//     if(target.closest('.modal__cancel') || target.classList.contains('modal')) {
+//         document.body.classList.remove('body--opened-modal')
+//     }
+// }
