bc4e77027a0a80e963ff31a2244ecf59fc0978fd
Pycha <daniil.kochev.2002@gmail.com>
2024-04-19 17:02:09 +0300

    program adaptive is finished. gallery is next

--- projects/wake-bake/wake-bake/js/main.js
+++ projects/wake-bake/wake-bake/js/main.js
@@ -1,5 +1,7 @@
 (function () {
 
+    // Burger
+
     document.addEventListener('click', burgerInit)
 
 
@@ -17,6 +19,8 @@
             document.body.classList.remove('body--opened-menu')
         }
     }
+
+    // Modal
 
     const modal = document.querySelector('.modal')
     const modalButton = document.querySelector('.about__img-button')
@@ -37,10 +41,75 @@
     })
 
     document.addEventListener('keydown', event => {
-        if(event.target.code = 'Escape' && document.body.classList.contains('body--opened-modal')) {
+        if (event.target.code = 'Escape' && document.body.classList.contains('body--opened-modal')) {
             document.body.classList.remove('body--opened-modal')
         }
     })
+
+    // Tabs
+
+    const tabControls = document.querySelector('.tab-controls')
+
+    tabControls.addEventListener('click', toggleTab) 
+
+    function toggleTab (e) {
+
+        const tabControl = e.target.closest('.tab-controls__link')
+
+        if(!tabControl) return
+        e.preventDefault()
+
+
+        const tabContentID = tabControl.getAttribute('href')
+        const tabContent = document.querySelector(tabContentID)
+        const activeControl = document.querySelector('.tab-controls__link--active')
+        const activeContent = document.querySelector('.tab-content--show')
+
+        if(activeControl) {
+            activeControl.classList.remove('tab-controls__link--active')
+        }
+
+        if(activeContent) {
+            activeContent.classList.remove('tab-content--show')
+        }
+
+        tabControl.classList.add('tab-controls__link--active')
+        tabContent.classList.add('tab-content--show')
+
+    }
+
+    // Accordion
+
+    const accordionLists = document.querySelectorAll('.accordion-list')
+
+    accordionLists.forEach(el => {
+
+        el.addEventListener('click', (e) => {
+
+            const accordionList = e.currentTarget
+            const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+            const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+            const accordionControl = e.target.closest('.accordion-list__control')
+
+            if(!accordionControl) return
+            const accordionItem = accordionControl.parentElement
+            const accordionContent = accordionControl.nextElementSibling
+
+            if(accordionOpenedItem && accordionItem != accordionOpenedItem){
+                accordionOpenedItem.classList.remove('accordion-list__item--opened')
+                accordionOpenedContent.style.maxHeight = null
+            }
+            accordionItem.classList.toggle('accordion-list__item--opened')
+
+            if(accordionItem.classList.contains('accordion-list__item--opened')) {
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px'
+            } else {
+                accordionContent.style.maxHeight = null
+            }
+
+        })
+
+    });
 
 })()
 
@@ -114,3 +183,94 @@
 //         document.body.classList.remove('body--opened-modal')
 //     }
 // }
+
+
+// ===================================== Tabs =====================================================
+
+
+//  // Tabs
+
+//  const tabControls = document.querySelector('.tab-controls')
+
+//  tabControls.addEventListener('click', toggleTab)
+
+//  function toggleTab(e) {
+
+//      const tabControl = e.target.closest('.tab-controls__link')
+
+//      if (!tabControl) return
+//      e.preventDefault()
+
+//      if (tabControl.classList.contains('tab-controls__link--active')) return
+
+//      const tabContentID = tabControl.getAttribute('href')
+
+//      console.log(tabControl.getAttribute('href'))
+
+//      document.querySelector('.tab-content--show').classList.remove('tab-content--show')
+//      document.querySelector(tabContentID).classList.add('tab-content--show')
+
+//      document.querySelector('.tab-controls__link--active').classList.remove('tab-controls__link--active')
+//      tabControl.classList.add('tab-controls__link--active')
+
+//  }
+
+
+// Tabs 
+
+// const tabControls = document.querySelector('.tab-controls')
+
+// tabControls.addEventListener('click', toggleTab)
+
+// function toggleTab(e) {
+
+//     const tabConrol = e.target.closest('.tab-controls__link')
+
+//     if (!tabConrol) return
+//     e.preventDefault()
+//     if (tabConrol.classList.contains('tab-controls__link--active')) return
+
+
+//     const tabContentID = tabConrol.getAttribute('href')
+//     const tabContent = document.querySelector(tabContentID)
+//     const activeControl = document.querySelector('.tab-controls__link--active')
+//     const activeContent = document.querySelector('.tab-content--show')
+
+//     if(activeControl) {
+//         activeControl.classList.remove('tab-controls__link--active')
+//     }
+//     if(activeContent) {
+//         activeContent.classList.remove('tab-content--show')
+//     }
+
+    
+//     tabConrol.classList.add('tab-controls__link--active')
+//     tabContent.classList.add('tab-content--show')
+
+
+// }
+
+
+// Accordion
+
+// const accordionLists = document.querySelectorAll('.accordion-list')
+
+// accordionLists.forEach(el => {
+
+//     el.addEventListener('click', (e) => {
+
+//         const accordionControl = e.target.closest('.accordion-list__control')
+//         if(!accordionControl) return
+//         const accordionItem = accordionControl.parentElement
+//         const accordionContent = accordionControl.nextElementSibling
+
+//         accordionItem.classList.toggle('accordion-list__item--opened')
+
+//         if(accordionItem.classList.contains('accordion-list__item--opened')) {
+//             accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px'
+//         } else {
+//             accordionContent.style.maxHeight = null
+//         }
+
+//     })
+// });
