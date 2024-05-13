02b1a76608a52c9647efee127147372e9057498a
Pycha228 <empty.339@mail.ru>
2024-05-14 00:10:29 +0300

    added format script

--- coca/vite.config.js
+++ coca/vite.config.js
@@ -1,33 +1,33 @@
 import { defineConfig } from 'vite';
 import injectHTML from 'vite-plugin-html-inject';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-import { ViteMinifyPlugin } from 'vite-plugin-minify'
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 export default defineConfig({
-    root: './src',
-    server: {
-        port: 3000,
-        open: true,
+  root: './src',
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './src/index.html',
+      },
     },
-    build: {
-        outDir: '../dist',
-        emptyOutDir: true,
-        rollupOptions: {
-            input: {
-                main: './src/index.html',
-            }
-        }
-    },
-    plugins: [
-        ViteMinifyPlugin(),
-        injectHTML(),
-        ViteImageOptimizer({
-            png: {
-                quality: 80,
-            },
-            jpg: {
-                quality: 80,
-            },
-        }),
-    ],
+  },
+  plugins: [
+    ViteMinifyPlugin(),
+    injectHTML(),
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+    }),
+  ],
 });
