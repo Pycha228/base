7ffbd1c1fae8788524ada7a45ba2be741d941a44
Pycha228 <empty.339@mail.ru>
2024-05-22 21:01:42 +0300

    Homework 4.16 is done. Added scrollbar to Insight section slider

--- coca/src/js/components/home/slider.js
+++ coca/src/js/components/home/slider.js
@@ -1,13 +1,21 @@
 import Swiper from 'swiper';
-import { Autoplay, Navigation } from 'swiper/modules';
+import { Autoplay, Navigation, Scrollbar} from 'swiper/modules';
 import 'swiper/css';
 
 export const useInsightSlider = () => {
   new Swiper('.insight__slider', {
+    modules: [Scrollbar],
     slidesPerView: 'auto',
     spaceBetween: 32,
     loop: true,
     centeredSlides: true,
+    scrollbar: {
+      el: '.insight__slider-scrollbar',
+      horizontalClass: '.insight__slider-scrollbar',
+      draggable: true,
+      dragClass: 'insight__slider-scrollbar-drag',
+      dragSize: 100,
+    },
     breakpoints: {
       993: {
         centeredSlides: false,
