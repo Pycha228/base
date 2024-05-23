bbce5a1e9f4ac6d0c3f61784ad39d73cac72a1d9
Pycha228 <empty.339@mail.ru>
2024-05-23 03:39:16 +0300

    Homework 4.19 is done Pricing/day saving 20%

--- coca/src/js/components/pricing/priceSwitcher.js
+++ coca/src/js/components/pricing/priceSwitcher.js
@@ -3,29 +3,43 @@ export const usePriceSwitcher = () => {
   const starterPrice = document.querySelector('[data-price="starter"]');
   const popularPrice = document.querySelector('[data-price="popular"]');
   const enterprisePrice = document.querySelector('[data-price="enterprise"]');
+  const setPeriod = document.querySelectorAll('[data-price="period"]');
 
   const priceList = {
     starter: {
-      default: 120,
+      default: 4,
       withSale: 100,
     },
     popular: {
-      default: 1680,
+      default: 56,
       withSale: 1400,
     },
     enterprise: {
-      default: 2520,
+      default: 84,
       withSale: 2100,
     },
   };
 
+  const periodList = {
+    plan: {
+      default: '/day',
+      withSale: '/mo',
+    },
+  };
+
   const setPricesWithSale = () => {
+    setPeriod.forEach((el) => {
+      el.textContent = periodList.plan.withSale;
+    });
     starterPrice.textContent = priceList.starter.withSale;
     popularPrice.textContent = priceList.popular.withSale;
     enterprisePrice.textContent = priceList.enterprise.withSale;
   };
 
   const setDefaultPrices = () => {
+    setPeriod.forEach((el) => {
+      el.textContent = periodList.plan.default;
+    });
     starterPrice.textContent = priceList.starter.default;
     popularPrice.textContent = priceList.popular.default;
     enterprisePrice.textContent = priceList.enterprise.default;
